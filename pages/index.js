ba4fcc5e3cc62bdf77962efdd86e323934391e4e
GekiteniusY <genkichi.yogi@gmail.com>
2024-01-06 22:24:46 +0900

    プリレンダリングの学習

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+// パラメータに allPostsData を追加
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,6 +17,30 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      {/* ブログの目次を表示する処理を追加 */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => {
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>;
+          })}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
