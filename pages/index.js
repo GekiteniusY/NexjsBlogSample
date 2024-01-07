ae016d9cbfe196efb7f47c6ece364917fe07875a
GekiteniusY <genkichi.yogi@gmail.com>
2024-01-07 13:22:05 +0900

    動的ルート、サーバレス機能についての学習完了。Vercel＋Next.jsを活用したWebアプリケーション開発入門のハンズオン完了 https://codezine.jp/article/corner/925

--- pages/index.js
+++ pages/index.js
@@ -20,16 +20,22 @@ export default function Home({ allPostsData }) {
         </p>
       </section>
       {/* ブログの目次を表示する処理を追加 */}
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>{date}</small>
+              <br />
+              {/* <br />
               {title}
               <br />
               {id}
               <br />
-              {date}
+              {date} */}
             </li>
           ))}
         </ul>
